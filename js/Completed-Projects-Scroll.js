4331e1037c01b92d22a9502100075f272fc4aae4
Mark Houston <markhouston95@gmail.com>
2021-07-13 16:25:21 +0100

    Install Project Upload

---
+++ js/Completed-Projects-Scroll.js
@@ -0,0 +1,11 @@
+$(document).ready(function (){
+    const scroll = new SmoothScroll('a[href*="#bedrooms"]', {
+        speed: 1000
+    });
+})
+
+$(document).ready(function (){
+    const scroll = new SmoothScroll('a[href*="#kitchens"]', {
+        speed: 800
+    });
+})
---
+++ js/scroll.js
@@ -0,0 +1,5 @@
+$(document).ready(function (){
+    const scroll = new SmoothScroll('a[href*="#scroll"]', {
+        speed: 800
+    });
+})
