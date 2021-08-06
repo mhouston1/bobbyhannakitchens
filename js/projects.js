798dc75a1bdd0883451461c6b146c0d69f0e1793
Mark Houston <markhouston95@gmail.com>
2021-08-06 19:22:42 +0100

    Update to Project pages and links

---
+++ js/projects.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class ModalComponent extends React.Component {
+    updateModalNumber(){
+        var modalNumber;
+
+        document.getElementById(Modal)
+    }
+
+}
