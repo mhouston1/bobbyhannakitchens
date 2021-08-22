47ac5f69f3a701f35a61670c782d6819d015a576
Mark Houston <markhouston95@gmail.com>
2021-08-22 15:16:24 +0100

    Updated Images Sizes and redesigned homepage

---
+++ js/index.js
@@ -0,0 +1,9 @@
+if(window.onload.width() <= 576) { 
+    $("#carousel-item-1").attr("src","../img/Slideshow/sm/BobbyHanna_Slideshow1.jpg");
+    $("#carousel-item-2").attr("src","../img/Slideshow/sm/BobbyHanna_Slideshow2.jpg");
+    $("#carousel-item-3").attr("src","../img/Slideshow/sm/BobbyHanna_Slideshow3.jpg");
+} else {
+    $("#carousel-item-1").attr("src","../img/Slideshow/BobbyHanna_Slideshow1.jpg");
+    $("#carousel-item-2").attr("src","../img/Slideshow/BobbyHanna_Slideshow2.jpg");
+    $("#carousel-item-3").attr("src","../img/Slideshow/BobbyHanna_Slideshow3.jpg");
+}
