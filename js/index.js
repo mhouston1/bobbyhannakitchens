8b37c8813c0cb33fbb98b268d1ad4be6dedebbc7
Mark Houston <markhouston95@gmail.com>
2021-08-23 20:09:06 +0100

    Update to index.html and general fixes

--- js/index.js
+++ js/index.js
@@ -1,9 +1,11 @@
-if(window.onload.width() <= 576) { 
-    $("#carousel-item-1").attr("src","../img/Slideshow/sm/BobbyHanna_Slideshow1.jpg");
-    $("#carousel-item-2").attr("src","../img/Slideshow/sm/BobbyHanna_Slideshow2.jpg");
-    $("#carousel-item-3").attr("src","../img/Slideshow/sm/BobbyHanna_Slideshow3.jpg");
-} else {
-    $("#carousel-item-1").attr("src","../img/Slideshow/BobbyHanna_Slideshow1.jpg");
-    $("#carousel-item-2").attr("src","../img/Slideshow/BobbyHanna_Slideshow2.jpg");
-    $("#carousel-item-3").attr("src","../img/Slideshow/BobbyHanna_Slideshow3.jpg");
-}
+$( window ).resize(function() {
+    if($( window ).width() < 576) { 
+        $("#carousel-item-1").attr("src","../img/Slideshow/sm/BobbyHanna_Slideshow1.jpg");
+        $("#carousel-item-2").attr("src","../img/Slideshow/sm/BobbyHanna_Slideshow2.jpg");
+        $("#carousel-item-3").attr("src","../img/Slideshow/sm/BobbyHanna_Slideshow3.jpg");
+    } else {
+        $("#carousel-item-1").attr("src","../img/Slideshow/BobbyHanna_Slideshow1.jpg");
+        $("#carousel-item-2").attr("src","../img/Slideshow/BobbyHanna_Slideshow2.jpg");
+        $("#carousel-item-3").attr("src","../img/Slideshow/BobbyHanna_Slideshow3.jpg");
+    }
+});
