e8342988584d589fe17c3d537e7300fbcc34adc1
Mark Houston <markhouston95@gmail.com>
2021-11-16 13:02:34 +0000

    Image update and YAML file

--- js/projects.js
+++ js/projects.js
@@ -3,9 +3,7 @@ import ReactDOM from 'react-dom';
 
 class ModalComponent extends React.Component {
     updateModalNumber(){
-        var modalNumber;
-
-        document.getElementById(Modal)
+        //logic goes in here
     }
 
 }
